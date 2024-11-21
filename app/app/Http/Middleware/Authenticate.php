c09c84c9446b7bb363255ed767764380cef3689e
Иван Каратуев <karatuevin@gmail.com>
2024-11-22 00:20:57 +0300

    Remove unused code

--- app/app/Http/Middleware/Authenticate.php
+++ app/app/Http/Middleware/Authenticate.php
@@ -3,14 +3,7 @@
 namespace App\Http\Middleware;
 
 use Closure;
-use DomainException;
-use Firebase\JWT\BeforeValidException;
-use Firebase\JWT\ExpiredException;
-use Firebase\JWT\JWT;
-use Firebase\JWT\Key;
-use Firebase\JWT\SignatureInvalidException;
 use Illuminate\Contracts\Auth\Factory as Auth;
-use Illuminate\Support\Facades\Log;
 
 class Authenticate
 {
@@ -41,21 +34,6 @@ class Authenticate
     public function handle($request, Closure $next, $guard = null)
     {
         return $next($request);
-        //        try {
-        //            if (array_key_exists('jwt',$_COOKIE)) {
-        //                $user = JWT::decode($_COOKIE['jwt'], new Key(env('SECRET_KEY'), env('ALG')));
-        //                if (date('Y-m-d H:i:s') > $user->info->logout) {
-        //                    return response('Unauthorized.', 401);
-        //                }
-        //                return $next($request);
-        //            } else {
-        //                throw new DomainException();
-        //            }
-        //        } catch (SignatureInvalidException | DomainException | BeforeValidException | ExpiredException $e) {
-        //            if ($request->path() == "cache")
-        //                return $next($request);
-        //            Log::info($e);
-        //            return response('Forbidden.', 405);
-        //        }
+
     }
 }
